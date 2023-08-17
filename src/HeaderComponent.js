d7c582462a752767d7bbd62984ee8b5d62d39f8e
poojapersani <poojaperasani@gmail.com>
2023-08-17 16:15:08 +0530

    modified header included header part and search functionality

--- src/HeaderComponent.js
+++ src/HeaderComponent.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import HomePageComponent from './HomePageComponent'
+import React,{useState, useEffect} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faSearch } from '@fortawesome/free-solid-svg-icons';
+import MovieCardRender from './MovieCardRender';
 
-const ParentComponent = () => {
-  return (
-    <div>
-      {/* Pass showSearch and showHome props */}
-      <HomePageComponent />
-      {/* Other content */}
-    </div>
-  );
-};
 
-export default ParentComponent;
+const apiKey = process.env.REACT_APP_KEY_NAME;
+
+    const Header = ({showHome, showSearch, showDetails}) => {
+      const [searchQuery, setSearchQuery] = useState('');
+      const [searchResults, setSearchResults] = useState([]);
+      /**Everytime user inputs search query this effect helps to fetch the results */
+      useEffect(() => {
+        if (searchQuery.trim() === '') {
+          setSearchResults([]);
+          return;
+        }
+        const fetchSearchResults = async () => {
+          try {
+            const response = await fetch(
+              `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`
+            );
+            const data = await response.json();
+            setSearchResults(data.results);
+          } catch (error) {
+            console.log('Unable to fetch search results:', error);
+          }
+        };
+        fetchSearchResults();
+      }, [searchQuery]);
+    
+      return (
+        <>
+            <div className="header">
+                {showSearch && <div className="search-bar">
+                    <FontAwesomeIcon icon={faSearch} className="search-button" />
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        value={searchQuery}
+                        onChange={(event) => setSearchQuery(event.target.value)}
+                    />
+                </div>}
+                {showDetails && <div className='details'>Movie Details</div>}
+                {showHome && <div className="home">     
+                    <a href='/'><FontAwesomeIcon icon={faHome} size="2x" color='black'/></a>
+                </div>}
+            </div>
+            <MovieCardRender movies={searchResults} /> 
+        </>
+      );
+    };
+export default Header;
